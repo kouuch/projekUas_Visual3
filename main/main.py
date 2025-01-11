819ea0005a740898dcca99140195c18c86082820
kouuch <rifkyirsada2@gmail.com>
2025-01-11 15:54:53 +0800

    link konek ke laporan dan log out dari main dashboard

--- main/main.py
+++ main/main.py
@@ -4,7 +4,7 @@ from getstarted import Ui_Form
 from Login import Ui_Form as Ui_Login
 from mainDashboard import Ui_Dialog as Ui_mainDashboard
 from mahasiswaDashboard import Ui_Dialog as Ui_mahasiswaDashboard
-
+from laporanDashboard import Ui_Dialog as Ui_laporan
 class MainWindow(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
@@ -38,6 +38,8 @@ class MainWindow(QMainWindow, Ui_Form):
         self.ui_window.setupUi(self.dasboard_window)
         
         self.ui_window.dmahasiswabtn.clicked.connect(self.show_mahasiswa_dashboard)
+        self.ui_window.laporanbtn.clicked.connect(self.show_laporan_dashboard)
+        self.ui_window.logoutbtn.clicked.connect(self.logout)
         self.dasboard_window.show()
 
     def show_mahasiswa_dashboard(self):
@@ -47,6 +49,17 @@ class MainWindow(QMainWindow, Ui_Form):
         self.ui_mahasiswa_dashboard_window.setupUi(self.mahasiswa_dashboard_window)
         
         self.mahasiswa_dashboard_window.show()
+
+    def show_laporan_dashboard(self):
+        self.dasboard_window.hide()
+        self.laporan_dashboard_window = QWidget()
+        self.ui_laporan_dashboard_window = Ui_laporan()
+        self.ui_laporan_dashboard_window.setupUi(self.laporan_dashboard_window)
+
+        self.laporan_dashboard_window.show()
+
+    def logout(self):
+        QApplication.quit()
 
 
 if __name__ == "__main__":
