9464d9a8734be23c987cfe14fe35eeea29287ab7
kouuch <rifkyirsada2@gmail.com>
2025-01-12 19:27:11 +0800

    logika penyimpanan excel

---
+++ main/exportexcel.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+from PyQt5.QtWidgets import QMessageBox
+from database import get_mahasiswa
+
+def export_to_excel(self):
+    mahasiswa_list = get_mahasiswa()
+
+    data = {
+        "Id Mahasiswa": [],
+        "Nama": [],
+        "NPM": [],
+        "Jurusan": [],
+        "Alamat": []
+    }
+
+    for row_data in mahasiswa_list:
+        data["Id Mahasiswa"].append(row_data[0])
+        data["Nama"].append(row_data[1])
+        data["NPM"].append(row_data[2])
+        data["Jurusan"].append(row_data[3])
+        data["Alamat"].append(row_data[4])
+
+    df = pd.DataFrame(data)
+
+    file_path = "data_mahasiswa.xlsx"
+    try:
+        df.to_excel(file_path, index=False,engine='openpyxl')
+        QMessageBox.information(self,"Informasi", f"Data berhasil diekpor ke {file_path}")
+    except Exception as e:
+        QMessageBox.warning(self, "Informasi",f"Gagal mengekspor dataa le Excel: {e}")
